a281098dae23e3fc9523885ec58146ef7afa01c1
Michael_windows <maicolcaceres14@gmail.com>
2024-11-19 12:37:56 -0400

    Feat: Se crearon sedder para los roles y se crearon los menus para el sistema

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -32,9 +32,9 @@ class UsuarioSeeder extends Seeder
         $usuario->usuario = 'admin';
         $usuario->password = Hash::make('rodry');
         $usuario->ci = '10028685';
-        $usuario->nombres = 'Michael';
-        $usuario->paterno = 'caceres';
-        $usuario->materno = 'quina';
+        $usuario->nombres = 'wilmer';
+        $usuario->paterno = 'villarroel';
+        $usuario->materno = 'surco';
         $usuario->estado = 'activo';
         $usuario->email = 'rodrigo@gmail.com';
         $usuario->save();
@@ -42,14 +42,43 @@ class UsuarioSeeder extends Seeder
         $usuario->syncRoles(['administrador']);
 
 
-        Permission::create(['name' => 'inicio.index'])->assignRole($rol1);
+        Permission::create(['name' => 'inicio.index'])->syncRoles([$rol1 ,$rol2]);
 
-        Permission::create(['name' => 'admin.index'])->syncRoles([$rol1,$rol2]);
+        Permission::create(['name' => 'admin.index'])->syncRoles([$rol1, $rol2]);
 
-
-        Permission::create(['name' => 'admin.usuario.incio'])->syncRoles([$rol1,$rol2]);
+        // USAURIO
+        Permission::create(['name' => 'admin.usuario.inicio'])->syncRoles([$rol1]);
         Permission::create(['name' => 'admin.usuario.crear'])->assignRole($rol1);
         Permission::create(['name' => 'admin.usuario.desactivar'])->assignRole($rol1);
         Permission::create(['name' => 'admin.usuario.reset'])->assignRole($rol1);
+        Permission::create(['name' => 'admin.usuario.edit'])->assignRole($rol1);
+
+
+        //ROL
+        Permission::create(['name' => 'admin.rol.inicio'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.rol.crear'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.rol.editar'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.rol.eliminar'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.rol.visualizar'])->syncRoles([$rol1]);
+
+        //PERMISOS
+        Permission::create(['name' => 'admin.permiso.inicio'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.permiso.crear'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.permiso.editar'])->syncRoles([$rol1]);
+        Permission::create(['name' => 'admin.permiso.eliminar'])->syncRoles([$rol1]);
+
+        // AFILIADOS
+        Permission::create(['name' => 'afiliado.index'])->syncRoles([$rol1]);
+
+        // ENCUESTAS
+        Permission::create(['name' => 'encuestas.index'])->syncRoles([$rol1]);
+
+
+         // FORMULARIO
+         Permission::create(['name' => 'formulario.index'])->syncRoles([$rol1]);
+
+        //  DISTRITO Y COMUNIDAD
+         Permission::create(['name' => 'distrito_comunidad.index'])->syncRoles([$rol1]);
+
     }
 }
--- resources/views/plantilla_admin/menu.blade.php
+++ resources/views/plantilla_admin/menu.blade.php
@@ -44,36 +44,106 @@
                             </a>
                             <div class="collapse " id="usuarios">
                                 <ul class="nav flex-column">
-                                    @can('admin.usuario.incio')
+                                    @can('admin.usuario.inicio')
                                         <li class="nav-item">
                                             <a class="nav-link" href="{{ route('usuarios.index') }}">Usuarios</a>
                                         </li><!--end nav-item-->
                                     @endcan
-                                    
+
+                                    @can('admin.rol.inicio')
                                         <li class="nav-item">
                                             <a class="nav-link" href="{{ route('roles.index') }}">Roles</a>
                                         </li><!--end nav-item-->
-                                    
-                                    
+                                    @endcan
+
+                                    @can('admin.permiso.inicio')
                                         <li class="nav-item">
                                             <a class="nav-link" href="{{ route('permisos.index') }}">Permisos</a>
                                         </li><!--end nav-item-->
-                                    
+                                    @endcan
 
                                 </ul><!--end nav-->
                             </div><!--end startbarApplications-->
                         </li><!--end nav-item-->
                     @endcan
 
-
-
-                    <li class="menu-label mt-2">
-                        <small class="label-border">
-                            <div class="border_left hidden-xs"></div>
-                            <div class="border_right"></div>
-                        </small>
-                        <span>NEGOCIO</span>
+                    <li class="menu-label pt-0 mt-0">
+                        <span>ACTIVIDADES</span>
                     </li>
+
+                    @can('afiliado.index')
+                        <li class="nav-item">
+                            <a class="nav-link" href="#afiliados" role="button" aria-expanded="false"
+                                aria-controls="sidebarDashboards">
+                                <i class="fas fa-users menu-icon"></i>
+                                <span>AFILIADOS</span>
+                            </a>
+                        </li>
+                    @endcan
+
+                    @can('encuestas.index')
+                        <li class="nav-item">
+                            <a class="nav-link" href="#encuestas" data-bs-toggle="collapse" role="button"
+                                aria-expanded="false" aria-controls="encuestas">
+                                <i class="far fa-list-alt menu-icon"></i>
+                                <span>ENCUESTAS</span>
+                            </a>
+                            <div class="collapse " id="encuestas">
+                                <ul class="nav flex-column">
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#encuestas">Usuarios</a>
+                                    </li><!--end nav-item-->
+
+                                </ul>
+                            </div>
+                        </li>
+                    @endcan
+
+                    @can('formulario.index')
+                        <li class="nav-item">
+                            <a class="nav-link" href="#formulario" data-bs-toggle="collapse" role="button"
+                                aria-expanded="false" aria-controls="formulario">
+                                <i class="fas fa-clipboard-list menu-icon"></i>
+                                <span>FORMULARIO</span>
+                            </a>
+                            <div class="collapse " id="formulario">
+                                <ul class="nav flex-column">
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#formulario">Usuarios</a>
+                                    </li><!--end nav-item-->
+
+                                </ul>
+                            </div>
+                        </li>
+                    @endcan
+
+
+                    @can('distrito_comunidad.index')
+                        <li class="menu-label pt-0 mt-0">
+                            <span>LOCALIDAD</span>
+                        </li>
+                    @endcan
+
+                    @can('distrito_comunidad.index')
+                        <li class="nav-item">
+                            <a class="nav-link" href="#localidad" data-bs-toggle="collapse" role="button"
+                                aria-expanded="false" aria-controls="formulario">
+                                <i class="fas fa-map-marker-alt menu-icon"></i>
+                                <span>DISTRITO Y COMUNIDAD</span>
+                            </a>
+                            <div class="collapse " id="localidad">
+                                <ul class="nav flex-column">
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#localidad">Distrito y Comunidad</a>
+                                    </li><!--end nav-item-->
+
+                                </ul>
+                            </div>
+                        </li>
+                    @endcan
                     {{-- <li class="nav-item">
                         <a class="nav-link" href="#sidebarElements" data-bs-toggle="collapse" role="button"
                             aria-expanded="false" aria-controls="sidebarElements">
